94788b68254ac5f3756bc827d6a5d906d6fabcee
Daniel Teo <danieltatloongteo@outlook.com>
2021-09-25 12:58:24 -0700

    Added recipe component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+// Temporary placeholder recipe
+const placeholderRecipe = {
+  recipeName: "Minced Beef",
+  recipePreparationTime: 40,
+  recipeCostPerServing: 7.99,
+  recipeCalories: 700,
+  recipeImgUrl:
+    "https://www.safefood.net/getmedia/7cd50be3-8aca-40d0-b3d0-9520561f5a23/mince_1.jpg?w=2048&h=1152&ext=.jpg&width=1360&resizemode=force",
+};
+
 const NavBar = () => {
   return (
     <nav className="navBar">
@@ -15,13 +25,38 @@ const SearchBar = () => {
   return <input className="searchBar" placeholder="what's cooking..." />;
 };
 
+const Recipe = (props) => {
+  return (
+    <div className="recipe">
+      <img
+        className="recipe image"
+        src={props.recipeObj.recipeImgUrl}
+        alt="Recipe"
+      />
+      <h2 className="recipe name">{props.recipeObj.recipeName}</h2>
+      <div className="recipe overview">
+        <h4>Preparation Time: </h4>
+        <p>{props.recipeObj.recipePreparationTime} minutes</p>
+        <h4>Cost Per Serving: </h4>
+        <p>${props.recipeObj.recipeCostPerServing}</p>
+        <h4>Calories Per Serving: </h4>
+        <p>{props.recipeObj.recipeCalories} calories</p>
+      </div>
+      <h3 className="recipe ingredients">Ingredients</h3>
+      <h3 className="recipe instructions">Instructions</h3>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
       <NavBar />
       <div className="searchContainer">
         <SearchBar />
-        <div className="recipeContainer"></div>
+      </div>
+      <div className="recipeContainer">
+        <Recipe recipeObj={placeholderRecipe} />
       </div>
     </div>
   );
