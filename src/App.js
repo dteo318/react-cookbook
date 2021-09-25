512462b1bf9a5cd33fe2aaff8b43c29c3ebc2de0
Daniel Teo <danieltatloongteo@outlook.com>
2021-09-25 16:20:04 -0700

    Added recipe details page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // Temporary placeholder recipe
 const placeholderRecipe = {
   recipeName: "Minced Beef",
@@ -6,6 +8,30 @@ const placeholderRecipe = {
   recipeCalories: 700,
   recipeImgUrl:
     "https://www.safefood.net/getmedia/7cd50be3-8aca-40d0-b3d0-9520561f5a23/mince_1.jpg?w=2048&h=1152&ext=.jpg&width=1360&resizemode=force",
+  recipeInstructions: [
+    "Step 1",
+    "Step 2",
+    "Step 3",
+    "Step 4",
+    "Step 5",
+    "Step 6",
+    "Step 7",
+    "Step 8",
+    "Step 9",
+    "Step 10",
+  ],
+  recipeIngredients: [
+    "Ingredient 1",
+    "Ingredient 2",
+    "Ingredient 3",
+    "Ingredient 4",
+    "Ingredient 5",
+    "Ingredient 6",
+    "Ingredient 7",
+    "Ingredient 8",
+    "Ingredient 9",
+    "Ingredient 10",
+  ],
 };
 
 const NavBar = () => {
@@ -26,6 +52,18 @@ const SearchBar = () => {
 };
 
 const Recipe = (props) => {
+  const [viewingDetails, setViewingDetails] = useState(false);
+
+  if (viewingDetails) {
+    return (
+      <RecipeDetails
+        onReturnToOverview={() => setViewingDetails(!viewingDetails)}
+        ingredients={props.recipeObj.recipeIngredients}
+        instructions={props.recipeObj.recipeInstructions}
+      />
+    );
+  }
+
   return (
     <div className="recipe">
       <img
@@ -34,17 +72,55 @@ const Recipe = (props) => {
         alt="Recipe"
       />
       <h2 className="recipe name">{props.recipeObj.recipeName}</h2>
-      <div className="recipe overview">
-        <h4>Preparation Time: </h4>
-        <p>{props.recipeObj.recipePreparationTime} minutes</p>
-        <h4>Cost Per Serving: </h4>
-        <p>${props.recipeObj.recipeCostPerServing}</p>
-        <h4>Calories Per Serving: </h4>
-        <p>{props.recipeObj.recipeCalories} calories</p>
+      <RecipeOverview
+        recipePreparationTime={props.recipeObj.recipePreparationTime}
+        recipeCostPerServing={props.recipeObj.recipeCostPerServing}
+        recipeCalories={props.recipeObj.recipeCalories}
+      />
+      <h3
+        onClick={() => setViewingDetails(!viewingDetails)}
+        className="recipe details button"
+      >
+        Details
+      </h3>
+    </div>
+  );
+};
+
+const RecipeOverview = (props) => {
+  return (
+    <div className="recipe overview">
+      <h4>Preparation Time: </h4>
+      <p>{props.recipePreparationTime} minutes</p>
+      <h4>Cost Per Serving: </h4>
+      <p>${props.recipeCostPerServing}</p>
+      <h4>Calories Per Serving: </h4>
+      <p>{props.recipeCalories} calories</p>
+    </div>
+  );
+};
+
+const RecipeDetails = (props) => {
+  return (
+    <div className="recipe details">
+      <p className="recipe returnOverview" onClick={props.onReturnToOverview}>
+        Go back to overview
+      </p>
+      <div className="recipe ingredients">
+        <h2>Ingredients</h2>
+        <ul>
+          {props.ingredients.map((ingredient, idx) => (
+            <li>{ingredient}</li>
+          ))}
+        </ul>
       </div>
-      <div className="recipe details">
-        <h3 className="recipe ingredients">Ingredients</h3>
-        <h3 className="recipe instructions">Instructions</h3>
+      <div className="recipe instructions">
+        <h2>Instructions</h2>
+        <ol>
+          {props.instructions.map((instructions, idx) => (
+            <li>{instructions}</li>
+          ))}
+        </ol>
       </div>
     </div>
   );
