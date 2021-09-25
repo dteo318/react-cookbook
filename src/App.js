ef89d4dfc300c82c553697f1e2da42980e2c8814
Daniel Teo <danieltatloongteo@outlook.com>
2021-09-25 12:15:49 -0700

    Added navigation and seach bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+      <h1>My Cookbook</h1>
+      <ul>
+        <li>Find Recipes</li>
+        <li>Saved Recipes</li>
+        <li>Costs & Calories</li>
+      </ul>
+    </nav>
+  );
+};
+
+const SearchBar = () => {
+  return <input className="searchBar" placeholder="what's cooking..." />;
+};
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <NavBar />
+      <div className="searchContainer">
+        <SearchBar />
+        <div className="recipeContainer"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
