6baa8470b1dc824621301000adedbbc3891f5176
Daniel Teo <danieltatloongteo@outlook.com>
2021-09-26 18:12:42 -0700

    User can save recipes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import Keys from "./config.js";
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <nav className="navBar">
       <h1>My Cookbook</h1>
       <ul>
-        <li>Find Recipes</li>
-        <li>Saved Recipes</li>
+        <li onClick={() => props.changePage("searchRecipes")}>Find Recipes</li>
+        <li onClick={() => props.changePage("savedRecipes")}>Saved Recipes</li>
         <li>Costs & Calories</li>
       </ul>
     </nav>
   );
 };
 
-const getRecipes = (query) => {
+const getRecipes = (query, savedRecipes) => {
   const searchRecipeUrl =
     `https://api.spoonacular.com/recipes/complexSearch?apiKey=${Keys.api_key}` +
     `&query=${query}&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true&fillIngredients=true`;
@@ -29,30 +29,52 @@ const getRecipes = (query) => {
       for (let i = 0; i < results.length; i++) {
         const result = results[i];
         try {
-          const recipe = {
-            recipeId: result.id,
-            recipeName: result.title,
-            recipePreparationTime: result.readyInMinutes,
-            recipeCostPerServing: result.pricePerServing,
-            recipeCalories: result.nutrition.nutrients[0].amount,
-            recipeImgUrl: result.image,
-            recipeInstructions: result.analyzedInstructions[0].steps,
-            recipeIngredients: result.extendedIngredients,
-          };
+          let savedRecipeFound = false;
 
-          recipes.push(recipe);
+          for (let i = 0; i < savedRecipes.length; i++) {
+            const savedRecipe = savedRecipes[i];
+            if (
+              savedRecipe.recipeId === result.id &&
+              savedRecipe.recipeName === result.title
+            ) {
+              recipes.push(savedRecipe);
+              savedRecipeFound = true;
+              break;
+            }
+          }
+
+          if (!savedRecipeFound) {
+            const recipe = {
+              recipeSaved: false,
+              recipeId: result.id,
+              recipeName: result.title,
+              recipePreparationTime: result.readyInMinutes,
+              recipeCostPerServing: result.pricePerServing,
+              recipeCalories: result.nutrition.nutrients[0].amount,
+              recipeImgUrl: result.image,
+              recipeInstructions: result.analyzedInstructions[0].steps,
+              recipeIngredients: result.extendedIngredients,
+            };
+            recipes.push(recipe);
+          }
         } catch (e) {
           console.log("Unable to parse recipe for: ", result);
+          console.log("Error: " + e);
         }
       }
       return recipes;
     });
 };
 
-function handleOnSearchBarKeyPress(e, searchText, onSearchedRecipes) {
+function handleOnSearchBarKeyPress(
+  e,
+  searchText,
+  onSearchedRecipes,
+  savedRecipes
+) {
   if (e.key === "Enter") {
     console.log("Searching for " + searchText + "...");
-    getRecipes(searchText).then((searchedRecipes) => {
+    getRecipes(searchText, savedRecipes).then((searchedRecipes) => {
       console.log("Found: ", searchedRecipes);
       onSearchedRecipes(searchedRecipes);
     });
@@ -67,12 +89,36 @@ const SearchBar = (props) => {
       className="searchBar"
       placeholder="what's cooking..."
       onKeyPress={(e) =>
-        handleOnSearchBarKeyPress(e, searchText, props.onSearchedRecipes)
+        handleOnSearchBarKeyPress(
+          e,
+          searchText,
+          props.onSearchedRecipes,
+          props.savedRecipes
+        )
       }
       onChange={(e) => onSearchBarTextChange(e.target.value)}
     />
   );
 };
+
+function handleRecipeSaved(recipeObj, savedRecipes, saveRecipe) {
+  if (recipeObj.recipeSaved) {
+    recipeObj.recipeSaved = false;
+    saveRecipe(
+      savedRecipes.filter(
+        (savedRecipe) =>
+          savedRecipe.recipeName !== recipeObj.recipeName &&
+          savedRecipe.recipeId !== recipeObj.recipeId
+      )
+    );
+    console.log("Unsaved " + recipeObj.recipeName);
+    return;
+  }
+
+  recipeObj.recipeSaved = true;
+  saveRecipe([...savedRecipes, recipeObj]);
+  console.log("Saved " + recipeObj.recipeName);
+}
 
 const Recipe = (props) => {
   const [viewingDetails, setViewingDetails] = useState(false);
@@ -106,6 +152,19 @@ const Recipe = (props) => {
       >
         Details
       </p>
+      <i
+        className={
+          (props.recipeObj.recipeSaved ? "fas" : "far") +
+          " fa-heart fa-2x recipeHeartIcon"
+        }
+        onClick={() =>
+          handleRecipeSaved(
+            props.recipeObj,
+            props.savedRecipes,
+            props.saveRecipe
+          )
+        }
+      ></i>
     </div>
   );
 };
@@ -133,7 +192,7 @@ const RecipeDetails = (props) => {
         <h2>Ingredients</h2>
         <ul>
           {props.ingredients.map((ingredient, idx) => (
-            <li>{ingredient.original}</li>
+            <li key={idx}>{ingredient.original}</li>
           ))}
         </ul>
       </div>
@@ -141,7 +200,7 @@ const RecipeDetails = (props) => {
         <h2>Instructions</h2>
         <ol>
           {props.instructions.map((instructions, idx) => (
-            <li>{instructions.step}</li>
+            <li key={idx}>{instructions.step}</li>
           ))}
         </ol>
       </div>
@@ -151,20 +210,50 @@ const RecipeDetails = (props) => {
 
 function App() {
   const [recipes, onSearchedRecipes] = useState([]);
+  const [currentPage, onPageSelected] = useState("searchRecipes");
+  const [savedRecipes, onRecipeSaved] = useState([]);
 
-  return (
-    <div>
-      <NavBar />
-      <div className="searchContainer">
-        <SearchBar onSearchedRecipes={onSearchedRecipes} />
-      </div>
-      <div className="recipeContainer">
-        {recipes.map((recipe, idx) => (
-          <Recipe recipeObj={recipe} key={recipe.recipeId} />
-        ))}
-      </div>
-    </div>
-  );
+  switch (currentPage) {
+    case "searchRecipes":
+      return (
+        <div>
+          <NavBar changePage={onPageSelected} />
+          <div className="searchContainer">
+            <SearchBar
+              onSearchedRecipes={onSearchedRecipes}
+              savedRecipes={savedRecipes}
+            />
+          </div>
+          {recipes.map((recipe, idx) => (
+            <Recipe
+              recipeObj={recipe}
+              key={recipe.recipeId}
+              saveRecipe={onRecipeSaved}
+              savedRecipes={savedRecipes}
+            />
+          ))}
+        </div>
+      );
+    case "savedRecipes":
+      return (
+        <div>
+          <NavBar changePage={onPageSelected} />
+          <div className="recipeContainer">
+            {savedRecipes.map((savedRecipe, idx) => (
+              <Recipe
+                recipeObj={savedRecipe}
+                key={savedRecipe.recipeId}
+                saveRecipe={onRecipeSaved}
+                savedRecipes={savedRecipes}
+              />
+            ))}
+          </div>
+        </div>
+      );
+    default:
+      console.log("Loaded null page");
+      break;
+  }
 }
 
 export default App;
